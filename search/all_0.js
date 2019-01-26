f02958dce4172b932bc2458b72d5e805fd3fe0ef
Victor Porton <porton@narod.ru>
2019-01-26 16:46:13 +0200

    v0.1.0

--- search/all_0.js
+++ search/all_0.js
@@ -6,7 +6,9 @@ var searchData=
   ['libcomcom_5finit2',['libcomcom_init2',['../libcomcom_8h.html#ab4e16404f99a99ddec86f2601ce8a220',1,'libcomcom.h']]],
   ['libcomcom_5finit_5fstratum',['libcomcom_init_stratum',['../libcomcom_8h.html#aedcb4a01d5ba61d8aa3a88e22475094c',1,'libcomcom.h']]],
   ['libcomcom_5freset_5fdefault_5fterminate',['libcomcom_reset_default_terminate',['../libcomcom_8h.html#a6257bdd6e3d91629952f6165dc0d3a66',1,'libcomcom.h']]],
+  ['libcomcom_5freset_5fdefault_5fterminate2',['libcomcom_reset_default_terminate2',['../libcomcom_8h.html#a3e97506c781da0fa369d55c60d6eace0',1,'libcomcom.h']]],
   ['libcomcom_5frun_5fcommand',['libcomcom_run_command',['../libcomcom_8h.html#a4556dc7f71b330a6826763673b9cddb4',1,'libcomcom.h']]],
   ['libcomcom_5fset_5fdefault_5fterminate',['libcomcom_set_default_terminate',['../libcomcom_8h.html#a54897e6e677a40a033f6363395d63bff',1,'libcomcom.h']]],
+  ['libcomcom_5fset_5fdefault_5fterminate2',['libcomcom_set_default_terminate2',['../libcomcom_8h.html#ab34786b0e04e8c7be91476cdc001d272',1,'libcomcom.h']]],
   ['libcomcom_5fterminate',['libcomcom_terminate',['../libcomcom_8h.html#a24e5536c06f08d4593300abcc5be5ae2',1,'libcomcom.h']]]
 ];
